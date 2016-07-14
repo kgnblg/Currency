42e07bb29fb6d72b023d9ead74b707eed2445305
kgnblg <kgnblg@gmail.com>
2016-07-14 15:52:27 +0300

    JSON eklendi

---
+++ Doviz/Json.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+
+namespace Doviz
+{
+    class Json1
+    {
+        public static string BaglantiKur(string tur)
+        {
+            string token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwczovL2F1dGgucGF4aW11bS5jb20iLCJhdWQiOiJodHRwczovL2FwaS5wYXhpbXVtLmNvbSIsIm5iZiI6MTQ2ODMxMDQ5MiwiZXhwIjoxNDcyNjg4MDAwLCJzdWIiOiI0MzZiYWYwNy01ZTRmLTQyMDEtYjBlNS01Njk3NzUyZGI3NmUiLCJyb2xlIjoicGF4OmRldmVsb3BlciJ9.jnHkmkXThdVZAwzr38lgqi3ZnnXEM3fpY-XeZsQyfnw";
+            string apiurl = "http://api.dev.paximum.com/v1/currency/GetExchangeRates?basecurrency=" + tur + "&access_token=" + token;
+            string sonuc;
+
+            using (var webClient = new WebClient())
+            {
+                sonuc = webClient.DownloadString(apiurl);
+            }
+
+            return sonuc;
+        }
+
+        public static string [] JsonParcala(string veri)
+        {
+            JObject gelenveri = JObject.Parse(veri);
+            JArray jsondizisi = (JArray)gelenveri["currencyPairs"];
+            string[] dizi = new string[jsondizisi.Count];
+            int i = 0;
+
+            foreach (var cocukdugum in jsondizisi)
+            {
+                string para = cocukdugum["counterCurrency"].ToString();
+                string kur = cocukdugum["rate"].ToString();
+                dizi[i] = para + " - " + kur;
+                i++;
+            }
+
+            return dizi;
+        }
+    }
+}
--- Doviz/Program.cs
+++ Doviz/Program.cs
@@ -9,11 +9,14 @@ namespace Doviz
         {
             Doviz1 doviz = new Doviz1(500, "USD");
 
-            XmlDocument doc = Xml.BaglantiKur(doviz.Birim1);
-            string[] gelen = Xml.XmlParcala(doc);
+            string jsoncikti = Json1.BaglantiKur("USD");
+            string [] gelen = Json1.JsonParcala(jsoncikti);
+
+            //XmlDocument doc = Xml.BaglantiKur(doviz.Birim1);
+            //string[] gelen = Xml.XmlParcala(doc);
 
             Hesap hesap = new Hesap(doviz.Tutar,gelen);
-            hesap.HesaplaYaz();
+            hesap.Hesapla();
             Console.ReadLine();
 
         }
