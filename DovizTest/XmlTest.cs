b07be431dd65dd50488d021d3ca4a81924bce7c3
kgnblg <kgnblg@gmail.com>
2016-07-14 10:12:21 +0300

    XmlParcala metod hatasi giderildi.

--- DovizTest/XmlTest.cs
+++ DovizTest/XmlTest.cs
@@ -8,8 +8,6 @@ namespace DovizTest
     [TestFixture]
     public class XmlTest
     {
-        XmlDocument doc;
-
         [Test]
         public void BaglantiKur()
         {
@@ -18,7 +16,7 @@ namespace DovizTest
             string token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwczovL2F1dGgucGF4aW11bS5jb20iLCJhdWQiOiJodHRwczovL2FwaS5wYXhpbXVtLmNvbSIsIm5iZiI6MTQ2ODMxMDQ5MiwiZXhwIjoxNDcyNjg4MDAwLCJzdWIiOiI0MzZiYWYwNy01ZTRmLTQyMDEtYjBlNS01Njk3NzUyZGI3NmUiLCJyb2xlIjoicGF4OmRldmVsb3BlciJ9.jnHkmkXThdVZAwzr38lgqi3ZnnXEM3fpY-XeZsQyfnw";
             string apiurl = "http://api.dev.paximum.com/v1/currency/GetExchangeRates?basecurrency=USD&access_token=" + token;
             string sonuc;
-            doc = new XmlDocument();
+            XmlDocument doc = new XmlDocument();
 
             using (var webClient = new WebClient())
             {
@@ -36,11 +34,23 @@ namespace DovizTest
         [Test]
         public void XmlParcala()
         {
-            string[] gelenxml = Xml.XmlParcala(doc);
+            string token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwczovL2F1dGgucGF4aW11bS5jb20iLCJhdWQiOiJodHRwczovL2FwaS5wYXhpbXVtLmNvbSIsIm5iZiI6MTQ2ODMxMDQ5MiwiZXhwIjoxNDcyNjg4MDAwLCJzdWIiOiI0MzZiYWYwNy01ZTRmLTQyMDEtYjBlNS01Njk3NzUyZGI3NmUiLCJyb2xlIjoicGF4OmRldmVsb3BlciJ9.jnHkmkXThdVZAwzr38lgqi3ZnnXEM3fpY-XeZsQyfnw";
+            string apiurl = "http://api.dev.paximum.com/v1/currency/GetExchangeRates?basecurrency=USD&access_token=" + token;
+            string sonuc;
+            XmlDocument doc = new XmlDocument();
+
+            using (var webClient = new WebClient())
+            {
+                webClient.Headers.Add("Accept", "text/xml");
+                sonuc = webClient.DownloadString(apiurl);
+            }
+
+            doc.LoadXml(sonuc);
 
             XmlNode genelnode = doc.DocumentElement.SelectSingleNode("CurrencyPairs");
             XmlNodeList ozelnode = genelnode.SelectNodes("CurrencyPair");
-            string[] sonuc = new string[ozelnode.Count];
+            string[] sonucnode = new string[ozelnode.Count];
+            string[] gelenxml = Xml.XmlParcala(doc);
             int i = 0;
 
             foreach (XmlNode teknode in ozelnode)
@@ -48,11 +58,11 @@ namespace DovizTest
                 string para = teknode.SelectSingleNode("CounterCurrency").InnerText;
                 string kur = teknode.SelectSingleNode("Rate").InnerText;
 
-                sonuc[i] = para + " - " + kur;
+                sonucnode[i] = para + " - " + kur;
                 i++;
             }
         
-            Assert.AreSame(sonuc,gelenxml);
+            Assert.AreEqual(sonucnode,gelenxml);
         }
     }
 }
